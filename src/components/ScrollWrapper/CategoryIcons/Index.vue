86e1e05ce716b37e7150cbe45c61f17b4a5a1cfa
leahzheng <zan272905918@163.com>
2020-10-28 15:31:57 +0800

    iconList

---
+++ src/components/ScrollWrapper/CategoryIcons/Index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="icons-wrapper">
+        <category-icons-sub
+            v-for="(item,index) of itemList"
+            :key="index"
+            :bgColor=item.bgColor
+            :icon=item.icon
+            :field=item.field
+            :iconText=item.iconText
+        ></category-icons-sub>
+       
+    </div>
+</template>
+<script>
+import CategoryIconsSub from './Sub'
+import fieldDates from 'data/fields'
+export default {
+    name:'CategoryIcons',
+    components:{
+        CategoryIconsSub
+    },
+    data(){
+        return{
+           itemList:fieldDates 
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '~styles/mixin.scss';
+@import '~styles/variables.scss';
+    .icons-wrapper{
+        @include flex-row;
+        flex-wrap: wrap;
+        background-color: #fff;
+        border-bottom: 1px solid #ddd;
+    }
+</style>
---
+++ src/components/ScrollWrapper/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="scroll-wrapper" ref="wrapper">
+        <div class="scroll-content">
+            <category-icons></category-icons>
+        </div>
+       
+    </div>
+</template>
+<script>
+import BetterScroll from 'better-scroll';
+import CategoryIcons from './CategoryIcons/Index'
+export default {
+    name:'HomeScrollWrapper',
+    mounted(){
+        this.scroll = new BetterScroll(this.$refs.wrapper)
+    },
+    components:{
+        CategoryIcons
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
