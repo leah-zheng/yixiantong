2046a7aa2f1cfe6544566c7da8cf8af73002c643
leahzheng <zan272905918@163.com>
2020-11-01 16:07:26 +0800

    get searchDatas

--- src/models/search.js
+++ src/models/search.js
@@ -2,7 +2,29 @@ import { HTTP } from 'utils/http';
 import config from 'utils/config'
 
 class SearchModel extends HTTP {
-
+    searchAction(keyword,cityId){
+        return new Promise((resolve,reject)=>{
+            this.axiosPost({
+                url:this.config.API.SEARCH_ACTION,
+                data:{
+                    keyword,
+                    cityId
+                },
+                success(data){
+                    resolve({
+                        stutus:0,
+                        data
+                    })
+                },
+                error(error){
+                    resolve({
+                        stutus:-1,
+                        error
+                    })
+                }
+            })
+        })
+    }
 }
 
 export { SearchModel };
